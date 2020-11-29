0a5173c110d50846aea6ca2973e4129bb9268f47
jwhuang42 <69329751+jwhuang42@users.noreply.github.com>
2020-11-29 17:45:47 -0500

    Update App.js

--- project/client/src/components/App.js
+++ project/client/src/components/App.js
@@ -5,8 +5,8 @@ import {
 	Switch
 } from 'react-router-dom';
 import Dashboard from './Dashboard';
-// import Recommendations from './Recommendations';
-// import BestGenres from './BestGenres';
+import Recommendations from './Recommendations';
+import BestGenres from './BestGenres';
 
 export default class App extends React.Component {
 

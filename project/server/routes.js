b9aa27de88551717e6396d68af7a29a723d93e91
zhouzihan98 <61778912+zhouzihan98@users.noreply.github.com>
2020-11-29 21:10:10 -0500

    Update routes.js
    "finish" best airbnb, add 3 get functions and the query

--- project/server/routes.js
+++ project/server/routes.js
@@ -10,6 +10,79 @@ var connection = mysql.createPool(config);
 
 
 
+/* ---- Best Airbnb ---- */
+
+function getAccommodates(req, res) {
+	var query = `
+        SELECT DISTINCT accommodates
+        FROM airbnb_listings
+		WHERE accommodates > 0
+		ORDER BY accommodates;
+  	`;
+	connection.query(query, function(err, rows, fields) {
+		if (err) console.log(err);
+		else {
+		  	res.json(rows);
+		}
+	});
+}
+
+function getBeds(req, res) {
+	var query = `
+        SELECT DISTINCT beds
+        FROM airbnb_listings
+        WHERE beds > 0
+        ORDER BY beds;
+    `;
+    connection.query(query, function(err, rows, fields) {
+        if (err) console.log(err);
+        else {
+            res.json(rows);
+        }
+    });
+}
+
+function getRoomType(req, res) {
+	var query = `
+        SELECT DISTINCT RoomType
+        FROM airbnb_listings;
+    `;
+    connection.query(query, function(err, rows, fields) {
+        if (err) console.log(err);
+        else {
+            res.json(rows);
+        }
+    });
+}
+
+function bestAirbnb(req, res) {
+    var inputNeighbourhood = req.params.neighbourhood;
+    var inputAccomodates = req.params.accommodates;
+    var inputBeds = req.params.beds;
+    var inputRoomType = req.params.room_type;
+    var inputPriceLow = req.params.price_low;
+    var inputPriceHigh = req.params.price_high;
+    var query = `
+        SELECT name, accommodates, beds, price, review_scores_rating 
+        FROM airbnb_listings
+        WHERE host_neighbourhood = '${inputNeighbourhood}'
+            AND accommodates >= '${inputAccomodates}'
+            AND beds >= ${inputBeds}
+            AND room_type = '${inputRoomType}'
+            AND price > ${inputPriceLow} AND price < ${inputPriceHigh}
+        ORDER BY review_scores_rating DESC 
+        LIMIT 10;
+    `;
+    connection.query(query, function(err, rows, fields) {
+        if (err) console.log(err);
+        else {
+            console.log(rows);
+            res.json(rows);
+        }
+    });
+};
+
+
 
 // The exported functions, which can be accessed in index.js.
 module.exports = {

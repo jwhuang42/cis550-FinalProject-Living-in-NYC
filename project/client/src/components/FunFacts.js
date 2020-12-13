3ea338aa3577c0fc74477c4b4af2499ac548343f
Zihan Zhou <zhou_zihan@berkeley.edu>
2020-12-12 23:26:20 -0500

    convert space to tab

--- project/client/src/components/FunFacts.js
+++ project/client/src/components/FunFacts.js
@@ -23,85 +23,85 @@ export default class FunFacts extends React.Component {
 
 
 	showAvgRating() {
-        // Send an HTTP request to the server.
-        fetch("http://localhost:8081/funfacts/avgRating", {
-        	method: 'GET' // The type of HTTP request.
-        })
-        	.then(res => res.json()) // Convert the response data to a JSON.
-        	.then(avgRatingList => {
-            	if (!avgRatingList) return;
-            	console.log(avgRatingList); //displays your JSON object in the console
-	  			let avgRatingDivs = avgRatingList.map((avgRatingResults, i) =>
-	  				<FunFactsAvgRatingRow host_neighbourhood={avgRatingResults.host_neighbourhood} count={avgRatingResults.count} avg_rating={avgRatingResults.avg_rating} />
-	  			);
+		// Send an HTTP request to the server.
+		fetch("http://localhost:8081/funfacts/avgRating", {
+			method: 'GET' // The type of HTTP request.
+		})
+			.then(res => res.json()) // Convert the response data to a JSON.
+			.then(avgRatingList => {
+				if (!avgRatingList) return;
+				console.log(avgRatingList); //displays your JSON object in the console
+				let avgRatingDivs = avgRatingList.map((avgRatingResults, i) =>
+					<FunFactsAvgRatingRow host_neighbourhood={avgRatingResults.host_neighbourhood} count={avgRatingResults.count} avg_rating={avgRatingResults.avg_rating} />
+				);
 
-	            // Set the state of the movies list to the value returned by the HTTP response from the server.
-	            this.setState({
+				// Set the state of the movies list to the value returned by the HTTP response from the server.
+				this.setState({
 					header: [<div className="header"><strong>Neighbourhood</strong></div>,
 							 <div className="header"><strong>Count</strong></div>,
 							 <div className="header"><strong>Average Rating</strong></div>],
-	            	result: avgRatingDivs
-	            })
-        	})
-        	.catch(err => console.log(err))	// Print the error if there is one.
-    }
+					result: avgRatingDivs
+				})
+			})
+			.catch(err => console.log(err))	// Print the error if there is one.
+	}
 
 	showNewHosts() {
-        // Send an HTTP request to the server.
-        fetch("http://localhost:8081/funfacts/newHosts", {
-        	method: 'GET' // The type of HTTP request.
-        })
-        	.then(res => res.json()) // Convert the response data to a JSON.
-        	.then(newHostsList => {
-            	if (!newHostsList) return;
-            	console.log(newHostsList); //displays your JSON object in the console
-	  			let newHostsDivs = newHostsList.map((newHostsResults, i) =>
-	  				<FunFactsNewHostsRow year={newHostsResults.year} num={newHostsResults.num} />
-	  			);
+		// Send an HTTP request to the server.
+		fetch("http://localhost:8081/funfacts/newHosts", {
+			method: 'GET' // The type of HTTP request.
+		})
+			.then(res => res.json()) // Convert the response data to a JSON.
+			.then(newHostsList => {
+				if (!newHostsList) return;
+				console.log(newHostsList); //displays your JSON object in the console
+				let newHostsDivs = newHostsList.map((newHostsResults, i) =>
+					<FunFactsNewHostsRow year={newHostsResults.year} num={newHostsResults.num} />
+				);
 
-	            // Set the state of the movies list to the value returned by the HTTP response from the server.
-	            this.setState({
+				// Set the state of the movies list to the value returned by the HTTP response from the server.
+				this.setState({
 					header: [<div className="header"><strong>Year</strong></div>,
 							 <div className="header"><strong>Number</strong></div>],
-	            	result: newHostsDivs
-	            });
-        	})
-        	.catch(err => console.log(err))	// Print the error if there is one.
-    }
+					result: newHostsDivs
+				});
+			})
+			.catch(err => console.log(err))	// Print the error if there is one.
+	}
 
 	showNumMovies() {
-        // Send an HTTP request to the server.
-        fetch("http://localhost:8081/funfacts/numMovies", {
-        	method: 'GET' // The type of HTTP request.
-        })
-        	.then(res => res.json()) // Convert the response data to a JSON.
-        	.then(numMoviesList => {
-            	if (!numMoviesList) return;
-            	console.log(numMoviesList); //displays your JSON object in the console
-	  			let numMoviesDivs = numMoviesList.map((numMoviesResults, i) =>
-	  				<FunFactsNumMoviesRow neighborhood={numMoviesResults.neighborhood} Borough={numMoviesResults.Borough} num={numMoviesResults.num} />
-	  			);
+		// Send an HTTP request to the server.
+		fetch("http://localhost:8081/funfacts/numMovies", {
+			method: 'GET' // The type of HTTP request.
+		})
+			.then(res => res.json()) // Convert the response data to a JSON.
+			.then(numMoviesList => {
+				if (!numMoviesList) return;
+				console.log(numMoviesList); //displays your JSON object in the console
+				let numMoviesDivs = numMoviesList.map((numMoviesResults, i) =>
+					<FunFactsNumMoviesRow neighborhood={numMoviesResults.neighborhood} Borough={numMoviesResults.Borough} num={numMoviesResults.num} />
+				);
 
-	            // Set the state of the movies list to the value returned by the HTTP response from the server.
-	            this.setState({
+				// Set the state of the movies list to the value returned by the HTTP response from the server.
+				this.setState({
 					header: [<div className="header"><strong>Neighbourhood</strong></div>,
 							 <div className="header"><strong>Borough</strong></div>,
 							 <div className="header"><strong>Number</strong></div>],
-	            	result: numMoviesDivs
-	            })
-        	})
-        	.catch(err => console.log(err))	// Print the error if there is one.
-    }
+					result: numMoviesDivs
+				})
+			})
+			.catch(err => console.log(err))	// Print the error if there is one.
+	}
 
 	render() {
-    	return (
-        	<div className="Dashboard">
-	        	<PageNavbar active="dashboard" />
+		return (
+			<div className="Dashboard">
+				<PageNavbar active="dashboard" />
 
-	        	<br></br>
-	        	<div className="container movies-container">
-	            	<div className="jumbotron">
-	            		<div className="h5">
+				<br></br>
+				<div className="container movies-container">
+					<div className="jumbotron">
+						<div className="h5">
 							Which fun fact do you wang to know?
 						</div>
 						<div className="">
@@ -111,33 +111,33 @@ export default class FunFacts extends React.Component {
 						</div>
 
 						<br></br>
-	            		<div className="">
+						<div className="">
 							<button type="button" class="btninfo btn-info" onClick={this.showNewHosts}>
 								Number of New Hosts each Year
 							</button>
 						</div>
 
 						<br></br>
-	                	<div className="">
+						<div className="">
 							<button type="button" class="btninfo btn-info" onClick={this.showNumMovies}>
 								Number of Movie Scenes in each Neighbourhood
 							</button>
 						</div>
-	            	</div>
+					</div>
 
-	            	<br></br>
-	            	<div className="jumbotron">
-	            		<div className="movies-container">
-	                		<div className="movies-header">
+					<br></br>
+					<div className="jumbotron">
+						<div className="movies-container">
+							<div className="movies-header">
 								{this.state.header}
-	                		</div>
-	                		<div className="results-container" id="results">
-	                			{this.state.result}
 							</div>
-	            		</div>
-	            	</div>
-	        	</div>
-        	</div>
-    	);
-    }
+							<div className="results-container" id="results">
+								{this.state.result}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }

04119602b1ac0cf13d3e943ded7f8d162fb60d18
jwhuang42 <jwhuang11235@gmail.com>
2020-12-13 18:06:15 -0500

    add a mising query

--- project/mysqlQueries/queries.sql
+++ project/mysqlQueries/queries.sql
@@ -1,6 +1,6 @@
 -- Output Best Airbnb based on User Requirements --
 WITH airbnb AS (
-	SELECT n.id, n.picture_url, n.name, p.accommodates, p.beds, p.price, 
+	SELECT n.id, n.picture_url, n.name, p.accommodates, p.beds, p.price,
 		FLOOR(r.review_scores_rating) AS rating, l.latitude, l.longitude
 	FROM airbnb_name n, airbnb_property p, airbnb_review r, airbnb_host h, airbnb_place l
 	WHERE n.id = p.id AND n.id = r.id AND n.id = h.id AND n.id = l.id
@@ -12,7 +12,7 @@ WITH airbnb AS (
 ), crime_count AS (
 	SELECT a.id, FLOOR(COUNT(*) / 9) AS num_crimes
 	FROM airbnb a, crime c
-	WHERE POWER((a.latitude - c.latitude) * 111139, 2) 
+	WHERE POWER((a.latitude - c.latitude) * 111139, 2)
 		+ POWER((a.longitude - c.longitude) * 111139, 2) < POWER(800, 2)
 	GROUP BY a.id
 ), result AS (
@@ -25,6 +25,20 @@ WITH airbnb AS (
 SELECT picture_url, name, accommodates, beds, price, IFNULL(rating, 'N/A') AS rating, num_crimes
 FROM result;
 
+
+-- get the first 40 most popular filming places
+WITH popPlace AS(
+SELECT neighborhood , Borough , count(*) as num
+FROM movie_scene
+GROUP BY neighborhood , Borough
+ORDER BY num DESC
+LIMIT 10
+)
+SELECT m.latitude, m.longitude, m.film, m.imdb_link, m.neighborhood, m.Borough, m.scene_type
+FROM movie_scene m
+JOIN popPlace p ON (m.neighborhood, m.Borough) = (p.neighborhood, p.Borough)
+ORDER BY p.num DESC
+LIMIT 40;
 
 
 -- Number and Average_Rating of Airbnb for each Neighbourhood --
@@ -58,7 +72,7 @@ LIMIT 10;
 
 -- Output Best Hotel based on User Requirements
 WITH hotel_rating AS (
-	SELECT name, street_address, hotel_class, postal_code, AVG(price) AS price, AVG(service) AS service, 
+	SELECT name, street_address, hotel_class, postal_code, AVG(price) AS price, AVG(service) AS service,
 		AVG(cleanliness) AS cleanliness, AVG(value) AS value, AVG(location_score) AS location,
 		AVG(sleep_quality) AS sleep_quality, AVG(rooms) AS room, AVG(overall) AS overall
 	FROM hotel
@@ -67,10 +81,10 @@ WITH hotel_rating AS (
 	SELECT DISTINCT NEIGHBORHOOD, ZIPCODE
 	FROM zillow
 ), hotel_info AS (
-	SELECT DISTINCT h.name, h.street_address, t.NEIGHBORHOOD AS neighbourhood, h.price, 
-		IFNULL(h.hotel_class, 0) AS class, IFNULL(h.service, 0) AS service, 
-        IFNULL(h.cleanliness, 0) AS cleanliness, IFNULL(h.value, 0) AS value, 
-        IFNULL(h.location, 0) AS location, IFNULL(h.sleep_quality, 0) AS sleep_quality, 
+	SELECT DISTINCT h.name, h.street_address, t.NEIGHBORHOOD AS neighbourhood, h.price,
+		IFNULL(h.hotel_class, 0) AS class, IFNULL(h.service, 0) AS service,
+        IFNULL(h.cleanliness, 0) AS cleanliness, IFNULL(h.value, 0) AS value,
+        IFNULL(h.location, 0) AS location, IFNULL(h.sleep_quality, 0) AS sleep_quality,
         IFNULL(h.room, 0) AS room, h.overall
 	FROM hotel_rating h JOIN transformer t ON h.postal_code = t.ZIPCODE
 	ORDER BY name
@@ -88,4 +102,3 @@ WHERE neighbourhood = 'CHELSEA'
     AND room >= 4
 ORDER BY overall DESC
 LIMIT 10;
-

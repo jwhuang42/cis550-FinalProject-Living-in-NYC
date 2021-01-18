10c7547149fbe6fa9e85397f056426e7930d6b81
jwhuang42 <69329751+jwhuang42@users.noreply.github.com>
2021-01-18 15:39:10 -0500

    Update db-config.js

--- project/server/db-config.js
+++ project/server/db-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	host: "cis550proj.c9ev4dsckxvk.us-east-1.rds.amazonaws.com",
-	port: "3306",
-	user: "cis550proj",
-	password: "cis550projgroup19jwhuangzhigaojdhuzzihan",
-	database: "cis550proj"
+	host: <Your Host Address>,
+	port: <3306 by default>,
+	user: <Username>,
+	password: <Your Password>,
+	database: <database name>
 };
